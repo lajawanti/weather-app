68583a2adf8b18d591a9c1edd41c176f26a6cb3a
lajawanti <lajawanti.dhake@gmail.com>
2019-12-27 14:37:00 -0500

    .env and weather api configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
-
-import Weather from './app_component/weather.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
-  return (
-    <div className="App">
-        <Weather />
-    </div>
-  );
+import 'weather-icons/css/weather-icons.css';
+import Weather from './app_component/weather.component';
+import { render } from '@testing-library/react';
+
+//api call api.openweathermap.org/data/2.5/weather?q=London,uk
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+    this.getWeather();
+  }
+
+  getWeather = async() => {
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID =${API_KEY}')
+    const response = await api_call.json();
+    console.log(response);
+  }
+
+  render(){
+    return (
+      <div className="App">
+          <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/app_component/weather.component.js
+++ src/app_component/weather.component.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
+
+
 const Weather = () => {
     return(
         <div className = "container">
             <div className = "cards">
-                <h1>London</h1>        
+                <h1>London</h1>  
+                <h5 className = "py-4">
+                    <i className = "wi wi-day-sunny"></i>
+                </h5>  
+
+                <h1 className = "py-2">25&deg;</h1>
+
+                {/**show max and min Temp */}    
+                {minmaxTemp(24, 19)}
+                <p>Min &emsp; &emsp; &emsp; Max</p>
+                <h4 className="py-3">Slow rain</h4>
             </div>
         </div>
     );
 };
+
+function minmaxTemp(min, max) {
+    return (
+        <h3>
+            <span className= "px-4">{min}&deg;</span>
+            <span className= "px-4">{max}&deg;</span>
+        </h3>
+    );
+}
 
 export default Weather;
